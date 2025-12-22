bf1d7af798bc6cdafdae7ae6626439e05ab23f8c
FernandoGomesK <fernandogomest94@gmail.com>
2025-12-22 05:35:03 -0300

    Trying to fix an item erros

--- obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb3e65752d632768ab4ea600ab3d2b0b3169daf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d9af1f09865669567f5723ad720c5f73549050")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
