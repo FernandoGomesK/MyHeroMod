a0644156809558ce61dd9c3a62a6678e50f97a53
Fernando <fernandogomest94@gmail.com>
2026-01-09 07:08:48 -0400

    AssemblyInfo

--- obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c7f5d3f556a772ffdc5ef82aa6d3de41f3ace1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b86fde0651943dce4be52937a3f0ce8e27224e32")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
