779254495e4cdfe641b40b4df31d9e4d545bd197
Fernando <fernandogomest94@gmail.com>
2026-02-19 07:21:04 -0400

    removed skilldata

--- obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e77373a3815d589e45ec939fdb3b176a7db1c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7abd7d0e6fad30fdc8958d3e8ce5c5dda9cc2d26")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
