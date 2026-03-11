52f69a4a140ff8ad6ffcc87a913e0b5d2bb97d2d
Fernando <fernandogomest94@gmail.com>
2026-03-11 12:18:36 -0400

    fixed a whole lot of bakugou sprites

--- obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d7f6ef1f3defca88a23538a728ec772202aa335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb7db58997eb4cd227881d37d816a2f43624b9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
