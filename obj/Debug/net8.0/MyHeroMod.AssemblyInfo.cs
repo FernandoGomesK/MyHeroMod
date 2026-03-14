2f21413074dfc9a672e5155a317b2605d53e3ed0
FernandoGomesK <fernandogomest94@gmail.com>
2026-03-14 18:26:31 -0300

    altered debug stage and erasing buff

--- obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9dc61b4fcdb267b1541ff8db366ab2cbf67c059")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a9d12cd9af496fee774f40bee1c375dedf7023e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
