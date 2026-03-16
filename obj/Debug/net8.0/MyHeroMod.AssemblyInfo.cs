1f13bd9e5986827ecd983d69d290f16955443796
FernandoGomesK <fernandogomest94@gmail.com>
2026-03-15 23:40:03 -0300

    testing all for one

--- obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f21413074dfc9a672e5155a317b2605d53e3ed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f3d6b1bd82c6d842e4b068721b45fa1656a0a06")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
