f1767e6ff43c5f553bcadcb8cb07b9eff2f4fe19
Fernando <fernandogomest94@gmail.com>
2026-04-05 18:46:57 -0400

    All for one Should sync correctly now in multiplayer

--- content/Quirks/AllForOne/AllForOnePlayer.cs
+++ content/Quirks/AllForOne/AllForOnePlayer.cs
@@ -9,47 +9,66 @@ using Terraria.GameContent.Bestiary;
 using MyHeroMod.content.Quirks.OFA9th;
 using Terraria.ModLoader.IO;
 
-
-namespace MyHeroMod.content.Quirks.AllForOne;
-
-    public partial class AllForOnePlayer : ModPlayer, IQuirkResetter
+namespace MyHeroMod.content.Quirks.AllForOne
 {
-    public int quirkCounter = 0;   
-    public int maxQuirks = 0;
+    public partial class AllForOnePlayer : ModPlayer, IQuirkResetter
+    {
+        public int quirkCounter = 0;   
+        public int maxQuirks = 0;
 
-    
+        public List<QuirkType> InternalQuirks = new List<QuirkType>();
 
-    public void FullReset() => InternalQuirks.Clear();
-    public List<QuirkType> InternalQuirks = new List<QuirkType>();
-    
-
-    public void UnlockQuirks(){
-        var transPlayer = Player.GetModPlayer<TransformationPlayer>();
-
-
-        if (transPlayer.SelectedQuirk == QuirkType.AllForOne)
+        public void FullReset() 
         {
             
+            InternalQuirks.Clear(); // Clears the stolen quirks list
+            quirkCounter = 0; 
         }
-    
-    
-        }
-            public bool HasInternalQuirk(QuirkType type)
-        {
-            return InternalQuirks.Contains(type);
-    }
-
-        public override void SaveData(TagCompound tag)
-                {
-                    List<int> savedQuirks = new List<int>();
-                    foreach (var quirk in InternalQuirks)
-                    {
-                        savedQuirks.Add((int)quirk); // Converte o Enum para Número para poder salvar
-                    }
-                    tag["AfoStolenQuirks"] = savedQuirks;
-                }
 
         
+        public bool TryStealQuirk(QuirkType newQuirk)
+        {
+            if (InternalQuirks.Count < maxQuirks && !HasInternalQuirk(newQuirk))
+            {
+                InternalQuirks.Add(newQuirk);
+                return true; 
+            }
+            return false; 
+        }
+
+        public bool HasInternalQuirk(QuirkType type)
+        {
+            return InternalQuirks.Contains(type);
+        }
+
+        public int CurrentQuirkCount => InternalQuirks.Count;
+
+        // Setting max limits
+        public override void ResetEffects()
+        {
+            var transPlayer = Player.GetModPlayer<TransformationPlayer>();
+
+            maxQuirks = transPlayer.CurrentStage switch
+            {
+                QuirkStage.Initial => 2,
+                QuirkStage.Adequation => 3,
+                QuirkStage.Intermediate => 5,
+                QuirkStage.Advanced => 8,
+                QuirkStage.Final => 10,
+                _ => 15, 
+            };
+        }
+
+        public override void SaveData(TagCompound tag)
+        {
+            List<int> savedQuirks = new List<int>();
+            foreach (var quirk in InternalQuirks)
+            {
+                savedQuirks.Add((int)quirk); 
+            }
+            tag["AfoStolenQuirks"] = savedQuirks;
+        }
+
         public override void LoadData(TagCompound tag)
         {
             if (tag.ContainsKey("AfoStolenQuirks"))
@@ -58,63 +77,50 @@ namespace MyHeroMod.content.Quirks.AllForOne;
                 var savedQuirks = tag.GetList<int>("AfoStolenQuirks");
                 foreach (var quirkId in savedQuirks)
                 {
-                    InternalQuirks.Add((QuirkType)quirkId); // Converte o Número de volta para Enum
+                    InternalQuirks.Add((QuirkType)quirkId); 
                 }
-            }}
-            public int CurrentQuirkCount => InternalQuirks.Count;
-
-            public override void PreUpdate()
-            {
-                var transPlayer = Player.GetModPlayer<TransformationPlayer>();
-
-                    maxQuirks = transPlayer.CurrentStage switch
-            {
-                QuirkStage.Initial => 2,
-                QuirkStage.Adequation => 3,
-                QuirkStage.Intermediate => 5,
-                QuirkStage.Advanced => 8,
-                QuirkStage.Final => 10,
-                _ => 15, // Default
-            };
-                    
-                    
             }
+        }
 
-        // public override void CopyClientState(ModPlayer targetCopy)
-        // {
-        //     GearshiftPlayer clone = targetCopy as GearshiftPlayer;
-        //     clone.GearActivation = GearActivation;
+        public override void CopyClientState(ModPlayer targetCopy)
+        {
+            AllForOnePlayer clone = targetCopy as AllForOnePlayer;
+            clone.InternalQuirks = new List<QuirkType>(InternalQuirks); 
+        }
+
+        public override void SyncPlayer(int toWho, int fromWho, bool newPlayer)
+        {
+            ModPacket packet = Mod.GetPacket();
+            packet.Write((byte)MyHeroMod.MessageType.SyncAllForOne); 
+            packet.Write((byte)Player.whoAmI); 
             
-        // }
-
-        // public override void SyncPlayer(int toWho, int fromWho, bool newPlayer)
-        // {
-        //     ModPacket packet = Mod.GetPacket();
-        //     packet.Write((byte)MyHeroMod.MessageType.SyncGearshift); 
-        //     packet.Write((byte)Player.whoAmI); 
+            packet.Write((int)InternalQuirks.Count);
+            foreach (var quirk in InternalQuirks)
+            {
+                packet.Write((int)quirk);
+            }
             
-        //     packet.Write(GearActivation); 
-        //     packet.Send(toWho, fromWho);
-        // }
+            packet.Send(toWho, fromWho);
+        }
 
-        // public override void SendClientChanges(ModPlayer clientPlayer)
-        // {
-        //     GearshiftPlayer clone = clientPlayer as GearshiftPlayer;
-        //     if (GearActivation != clone.GearActivation)
-        //     {
-        //         ModPacket packet = Mod.GetPacket();
-        //         packet.Write((byte)MyHeroMod.MessageType.SyncGearshift);
-        //         packet.Write((byte)Player.whoAmI);
-        //         packet.Write(GearActivation);
-        //         packet.Send(-1, Player.whoAmI); 
-        //     }
-        // }
-
-        
-
-        
-        
-
-        
+        public override void SendClientChanges(ModPlayer clientPlayer)
+        {
+            AllForOnePlayer clone = clientPlayer as AllForOnePlayer;
+            
+            if (InternalQuirks.Count != clone.InternalQuirks.Count)
+            {
+                ModPacket packet = Mod.GetPacket();
+                packet.Write((byte)MyHeroMod.MessageType.SyncAllForOne);
+                packet.Write((byte)Player.whoAmI);
+                
+                packet.Write((int)InternalQuirks.Count);
+                foreach (var quirk in InternalQuirks)
+                {
+                    packet.Write((int)quirk);
+                }
+                
+                packet.Send(-1, Player.whoAmI); 
+            }
+        }
     }
-        
+}
