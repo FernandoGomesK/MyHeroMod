bab628a214b4b5c8c947424226dff8f8d3d67ff8
FernandoGomesK <fernandogomest94@gmail.com>
2026-04-05 14:35:26 -0300

    added summon for all for one Boss

--- obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a90db27c6a22ec5634fd1f7a3baf94f85ee0899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+408c02729ca00151179b8dfb08d0120a747b5d28")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
