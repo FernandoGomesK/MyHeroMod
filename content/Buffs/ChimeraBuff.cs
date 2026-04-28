047f3c6275a6dfc8dcd494a42e4bdda7118ecb90
Fernando <fernandogomest94@gmail.com>
2026-04-28 10:34:17 -0400

    adding the image to the Chimera buff, the mutant arms

--- content/Buffs/ChimeraBuff.cs
+++ content/Buffs/ChimeraBuff.cs
@@ -7,7 +7,7 @@ namespace MyHeroMod.content.Buffs
 {
     public class ChimeraBuff : ModBuff
     {
-        public override string Texture => "MyHeroMod/Assets/BuffImage/OneForAllFullCowling5Percent";
+        public override string Texture => "MyHeroMod/Assets/BuffImage/ChimeraBuff";
         public override void SetStaticDefaults()
         {
             Main.buffNoSave[Type] = true;
--- obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c122dea0ceb348f6b29d475c1aa87a6e7b55e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+467c32ea5a04a760f746173e1b626414d16e1ae5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
