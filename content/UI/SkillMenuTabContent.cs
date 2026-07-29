0dca757fd1c68ddd9e32d4c67d8c72b0a40b22a2
Fernando <fernandogomest94@gmail.com>
2026-07-29 07:54:26 -0400

    skills from the core mod that do not make part of a quirks kit dont appear anymore on the skill menu

--- content/UI/SkillMenuTabContent.cs
+++ content/UI/SkillMenuTabContent.cs
@@ -1,160 +1,169 @@
 
-using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.GameContent.UI.Elements;
-using Terraria.UI;
-using Terraria.Audio;
-using Terraria.ID;
-using System.Collections.Generic;
-using MyHeroMod.content.System;
-using KhacesCore.Content.System;
+    using Microsoft.Xna.Framework;
+    using Terraria;
+    using Terraria.GameContent.UI.Elements;
+    using Terraria.UI;
+    using Terraria.Audio;
+    using Terraria.ID;
+    using System.Collections.Generic;
+    using MyHeroMod.content.System;
+    using KhacesCore.Content.System;
 
-namespace MyHeroMod.content.UI
-{
-    public class SkillMenuTabContent : UIElement
+    namespace MyHeroMod.content.UI
     {
-        private UIText descriptionText;
-        
-        
-        private string selectedSkill = "None"; 
-
-        public SkillMenuTabContent()
+        public class SkillMenuTabContent : UIElement
         {
-            Width.Set(0, 1f);
-            Height.Set(0, 1f);
-
-            var player = Main.LocalPlayer.GetModPlayer<TransformationPlayer>();
-            player.UpdateUnlockedSkills();
-
-            UIPanel listPanel = new UIPanel();
-            listPanel.Width.Set(230, 0);
-            listPanel.Height.Set(-90, 1f); 
-            listPanel.Left.Set(10, 0);
-            listPanel.Top.Set(10, 0);
-            listPanel.BackgroundColor = new Color(20, 20, 40);
-            Append(listPanel);
-
-            UIList skillList = new UIList();
-            skillList.Width.Set(-20, 1f);
-            skillList.Height.Set(0, 1f);
-            listPanel.Append(skillList);
-
-            var scrollbar = new UIScrollbar();
-            scrollbar.SetView(100f, 1000f);
-            scrollbar.Height.Set(0, 1f);
-            scrollbar.HAlign = 1f;
-            listPanel.Append(scrollbar);
-            skillList.SetScrollbar(scrollbar);
-
+            private UIText descriptionText;
             
-            CreateSlotButton(player, "Slot 1 (Z)", 10, 1);
-            CreateSlotButton(player, "Slot 2 (X)", 70, 2);
-            CreateSlotButton(player, "Slot 3 (C)", 130, 3);
-            CreateSlotButton(player, "Slot 4 (V)", 190, 4);
-
             
-            UIPanel descPanel = new UIPanel();
-            descPanel.Width.Set(-20, 1f); 
-            descPanel.Height.Set(60, 0);
-            descPanel.Left.Set(10, 0);
-            descPanel.Top.Set(-70, 1f); 
-            Append(descPanel);
+            private string selectedSkill = "None"; 
 
-            descriptionText = new UIText("Select a skill from the list...", 0.7f);
-            descriptionText.HAlign = 0.5f;
-            descriptionText.VAlign = 0.5f;
-            descPanel.Append(descriptionText);
-
-            PopulateSkillList(player, skillList);
-        }
-
-        private void PopulateSkillList(TransformationPlayer player, UIList skillList)
-        {
-            foreach (var skillId in SkillLibrary.GetAllIds()) 
+            public SkillMenuTabContent()
             {
-                var skillInstance = SkillLibrary.GetSkill(skillId); 
-                if (skillInstance == null) continue;
+                Width.Set(0, 1f);
+                Height.Set(0, 1f);
+
+                var player = Main.LocalPlayer.GetModPlayer<TransformationPlayer>();
+                player.UpdateUnlockedSkills();
+
+                UIPanel listPanel = new UIPanel();
+                listPanel.Width.Set(230, 0);
+                listPanel.Height.Set(-90, 1f); 
+                listPanel.Left.Set(10, 0);
+                listPanel.Top.Set(10, 0);
+                listPanel.BackgroundColor = new Color(20, 20, 40);
+                Append(listPanel);
+
+                UIList skillList = new UIList();
+                skillList.Width.Set(-20, 1f);
+                skillList.Height.Set(0, 1f);
+                listPanel.Append(skillList);
+
+                var scrollbar = new UIScrollbar();
+                scrollbar.SetView(100f, 1000f);
+                scrollbar.Height.Set(0, 1f);
+                scrollbar.HAlign = 1f;
+                listPanel.Append(scrollbar);
+                skillList.SetScrollbar(scrollbar);
+
+                
+                CreateSlotButton(player, "Slot 1 (Z)", 10, 1);
+                CreateSlotButton(player, "Slot 2 (X)", 70, 2);
+                CreateSlotButton(player, "Slot 3 (C)", 130, 3);
+                CreateSlotButton(player, "Slot 4 (V)", 190, 4);
+
+                
+                UIPanel descPanel = new UIPanel();
+                descPanel.Width.Set(-20, 1f); 
+                descPanel.Height.Set(60, 0);
+                descPanel.Left.Set(10, 0);
+                descPanel.Top.Set(-70, 1f); 
+                Append(descPanel);
+
+                descriptionText = new UIText("Select a skill from the list...", 0.7f);
+                descriptionText.HAlign = 0.5f;
+                descriptionText.VAlign = 0.5f;
+                descPanel.Append(descriptionText);
+
+                PopulateSkillList(player, skillList);
+            }
+
+            private void PopulateSkillList(TransformationPlayer player, UIList skillList)
+            {
+                foreach (var skillId in SkillLibrary.GetAllIds()) 
+                {
+                    var skillInstance = SkillLibrary.GetSkill(skillId); 
+                    if (skillInstance == null) continue;
+
+                
+                    if (skillInstance is QuirkBaseSkill quirkSkill)
+                    {
+                        if (!quirkSkill.CheckUnlock(player))
+                        continue;
+                    }
+                    else
+                    {
+                     if (!skillInstance.CanUse(player.Player))
+                     continue;   
+                    }
+                        
+
+                    UIPanel button = new UIPanel();
+                    button.Width.Set(0, 1f);
+                    button.Height.Set(40, 0);
+                    button.BackgroundColor = new Color(60, 60, 100);
+
+                    UIText text = new UIText(skillInstance.Name, 0.7f);
+                    text.HAlign = 0.5f;
+                    text.VAlign = 0.5f;
+                    button.Append(text);
+
+                    button.OnMouseOver += (evt, elem) => button.BackgroundColor = new Color(80, 80, 140);
+                    button.OnMouseOut += (evt, elem) => button.BackgroundColor = new Color(60, 60, 100);
+                    button.OnLeftClick += (evt, elem) =>
+                    {
+                        selectedSkill = skillId; 
+                        descriptionText.SetText(skillInstance.Description);
+                        SoundEngine.PlaySound(SoundID.MenuTick);
+                    };
+
+                    skillList.Add(button);
+                }
+            }
+
+            private void CreateSlotButton(TransformationPlayer player, string baseLabel, float top, int slotNum)
+            {
+                UIPanel slotBtn = new UIPanel();
+                slotBtn.Width.Set(-260, 1f);
+                slotBtn.Height.Set(50, 0);
+                slotBtn.Left.Set(250, 0);
+                slotBtn.Top.Set(top, 0);
+                slotBtn.BackgroundColor = Color.DarkSlateBlue;
 
             
-                if (skillInstance is QuirkBaseSkill quirkSkill && !quirkSkill.CheckUnlock(player)) 
-                    continue;
+                string currentSkill = "None";
+                if (slotNum == 1) currentSkill = player.Slot1;
+                if (slotNum == 2) currentSkill = player.Slot2;
+                if (slotNum == 3) currentSkill = player.Slot3;
+                if (slotNum == 4) currentSkill = player.Slot4;
 
-                UIPanel button = new UIPanel();
-                button.Width.Set(0, 1f);
-                button.Height.Set(40, 0);
-                button.BackgroundColor = new Color(60, 60, 100);
-
-                UIText text = new UIText(skillInstance.Name, 0.7f);
-                text.HAlign = 0.5f;
-                text.VAlign = 0.5f;
-                button.Append(text);
-
-                button.OnMouseOver += (evt, elem) => button.BackgroundColor = new Color(80, 80, 140);
-                button.OnMouseOut += (evt, elem) => button.BackgroundColor = new Color(60, 60, 100);
-                button.OnLeftClick += (evt, elem) =>
+                string displayLabel = baseLabel;
+                if (currentSkill != "None")
                 {
-                    selectedSkill = skillId; 
-                    descriptionText.SetText(skillInstance.Description);
-                    SoundEngine.PlaySound(SoundID.MenuTick);
+                    var existingSkill = SkillLibrary.GetSkill(currentSkill);
+                    if (existingSkill != null)
+                    {
+                        displayLabel = $"{baseLabel}: {existingSkill.Name}";
+                    }
+                }
+
+                UIText slotText = new UIText(displayLabel);
+                slotText.HAlign = 0.5f;
+                slotText.VAlign = 0.5f;
+                slotBtn.Append(slotText);
+
+                slotBtn.OnLeftClick += (evt, elem) =>
+                {
+                    
+                    if (selectedSkill == "None")
+                    {
+                        Main.NewText("Select a skill first!", Color.Red);
+                        return;
+                    }
+
+                    var skillInstance = SkillLibrary.GetSkill(selectedSkill);
+                    if (slotNum == 1) player.Slot1 = selectedSkill;
+                    if (slotNum == 2) player.Slot2 = selectedSkill;
+                    if (slotNum == 3) player.Slot3 = selectedSkill;
+                    if (slotNum == 4) player.Slot4 = selectedSkill;
+
+                    Main.NewText($"Assigned {skillInstance.Name} to Slot {slotNum}!", Color.Green);
+                    
+                    slotText.SetText($"{baseLabel}: {skillInstance.Name}");
+                    SoundEngine.PlaySound(SoundID.MenuOpen);
                 };
 
-                skillList.Add(button);
+                Append(slotBtn);
             }
-        }
-
-        private void CreateSlotButton(TransformationPlayer player, string baseLabel, float top, int slotNum)
-        {
-            UIPanel slotBtn = new UIPanel();
-            slotBtn.Width.Set(-260, 1f);
-            slotBtn.Height.Set(50, 0);
-            slotBtn.Left.Set(250, 0);
-            slotBtn.Top.Set(top, 0);
-            slotBtn.BackgroundColor = Color.DarkSlateBlue;
-
-        
-            string currentSkill = "None";
-            if (slotNum == 1) currentSkill = player.Slot1;
-            if (slotNum == 2) currentSkill = player.Slot2;
-            if (slotNum == 3) currentSkill = player.Slot3;
-            if (slotNum == 4) currentSkill = player.Slot4;
-
-            string displayLabel = baseLabel;
-            if (currentSkill != "None")
-            {
-                var existingSkill = SkillLibrary.GetSkill(currentSkill);
-                if (existingSkill != null)
-                {
-                    displayLabel = $"{baseLabel}: {existingSkill.Name}";
-                }
-            }
-
-            UIText slotText = new UIText(displayLabel);
-            slotText.HAlign = 0.5f;
-            slotText.VAlign = 0.5f;
-            slotBtn.Append(slotText);
-
-            slotBtn.OnLeftClick += (evt, elem) =>
-            {
-                
-                if (selectedSkill == "None")
-                {
-                    Main.NewText("Select a skill first!", Color.Red);
-                    return;
-                }
-
-                var skillInstance = SkillLibrary.GetSkill(selectedSkill);
-                if (slotNum == 1) player.Slot1 = selectedSkill;
-                if (slotNum == 2) player.Slot2 = selectedSkill;
-                if (slotNum == 3) player.Slot3 = selectedSkill;
-                if (slotNum == 4) player.Slot4 = selectedSkill;
-
-                Main.NewText($"Assigned {skillInstance.Name} to Slot {slotNum}!", Color.Green);
-                
-                slotText.SetText($"{baseLabel}: {skillInstance.Name}");
-                SoundEngine.PlaySound(SoundID.MenuOpen);
-            };
-
-            Append(slotBtn);
         }
     }
-}
--- obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyHeroMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0ccb766f73c7a4af64c5baf649e1bcc7c2fd46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68590036a0550827a85a2c7c726b17605e78a8d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHeroMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
