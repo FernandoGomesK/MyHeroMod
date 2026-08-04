45c3e7165ba436c71d16e9cc485fbff2506cc46d
Fernando <fernandogomest94@gmail.com>
2026-08-04 19:12:16 -0400

    creating 1000000 detroit smash

---
+++ content/Quirks/OFA9th/OFA9List/Detroit1000000Smash.cs
@@ -0,0 +1,91 @@
+using Terraria;
+using Terraria.ModLoader;
+using MyHeroMod.content.System;
+using MyHeroMod.content;
+using Microsoft.Xna.Framework;
+using MyHeroMod.content.Quirks.OFA9th.Projectiles;
+using MyHeroMod.content.Quirks.AllForOne;
+using KhacesCore.Content.System;
+using MyHeroMod.content.Quirks.OFA9th;
+using MyHeroMod.content.Quirks.OFA8th;
+using MyHeroMod.content.Projectiles;
+using MyHeroMod.content.Buffs;
+
+namespace MyHeroMod.content.Quirks.OFA9th.Skills 
+{
+public class Detroit1000000SmashSkill : QuirkBaseSkill
+{
+    public override string Name => "Detroit Smash";
+    public override string Description => "Propel air forward with a massive punch";
+    public override string IconPath => "MyHeroMod/Assets/Skills/DelawareSmash";
+
+    public override int BaseCooldown => 120;
+    public override string Category => "OneForAll9th";
+
+    public override QuirkType RequiredQuirk => QuirkType.Quirkless;
+    public override QuirkStage RequiredStage => QuirkStage.Initial;
+    public override bool IsDefaultSkill => false;
+    public override bool IsBaseQuirk => false;
+
+    public override bool CheckUnlock(TransformationPlayer player)
+    {
+        var afoPlayer = player.Player.GetModPlayer<AllForOnePlayer>();
+
+        if (player.HasActiveQuirk(QuirkType.OneForAll8th) || player.HasActiveQuirk(QuirkType.OneForAll9th))
+            return player.CurrentStage >= QuirkStage.Initial;
+
+        if (player.HasActiveQuirk(QuirkType.AllForOne) && (afoPlayer.HasInternalQuirk(QuirkType.OneForAll8th) || afoPlayer.HasInternalQuirk(QuirkType.OneForAll9th)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override void OnUse(Player player)
+    {
+        
+        var transPlayer = player.GetModPlayer<TransformationPlayer>();
+        
+        
+        int onomatopoeiaType = ModContent.ProjectileType<DetroitOnomatopoeia>();
+
+        
+        if (transPlayer.HasActiveQuirk(QuirkType.OneForAll9th))
+        {
+            if (player.HasBuff(ModContent.BuffType<GearshiftBuff>()))
+            {
+                onomatopoeiaType = ModContent.ProjectileType<GearDekuDetroitOnomatopoeia>();
+            }
+            else
+            {
+                onomatopoeiaType = ModContent.ProjectileType<DekuDetroitOnomatopoeia>();
+            }
+            
+        }
+
+        
+        Vector2 textPosition = player.Center + new Vector2(0, -30f);
+        Projectile.NewProjectile(
+            player.GetSource_FromThis(),
+            textPosition,
+            Vector2.Zero, 
+            onomatopoeiaType, 
+            0, 
+            0f, 
+            player.whoAmI
+        );
+
+        
+        Projectile.NewProjectile(
+            player.GetSource_FromThis(), 
+            player.Center, 
+            Vector2.Zero, 
+            ModContent.ProjectileType<ChargeDetroitProj>(), 
+            0, 
+            0f, 
+            player.whoAmI
+        );
+    }
+}
+}
---
+++ content/Quirks/OFA9th/Projectiles/Charge1000000DetroitProj.cs
@@ -0,0 +1,79 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.Graphics.CameraModifiers; 
+using Terraria.DataStructures;
+using MyHeroMod.content.Buffs;
+using MyHeroMod.content.Quirks.FaJin;
+using MyHeroMod.content.Quirks.OFA8th;
+using MyHeroMod.content.Quirks.AllForOne;
+using KhacesCore.Content.System.BaseProjectiles;
+using MyHeroMod.content.Projectiles;
+
+
+namespace MyHeroMod.content.Quirks.OFA9th.Projectiles
+{ 
+    public class Charge1000000DetroitProj : BaseChannelingProj
+    {
+        public override string Texture => "MyHeroMod/content/Quirks/Explosion/Projectiles/HowitzerImpact/HowitzerImpactProj";
+
+        protected override int ChannelTime => 40; 
+
+        public override void AI()
+        {
+            base.AI(); 
+            Player player = Main.player[Projectile.owner];
+            
+            if (player.active && !player.dead)
+            {
+                player.velocity *= 0.6f; 
+            }
+
+            
+            if (Projectile.ai[0] == 1) 
+            {
+                SoundEngine.PlaySound(new SoundStyle("MyHeroMod/Assets/Sounds/FullCowlingActivationSound"), player.position);
+            }
+        }
+
+        public override void SpawnChargingDust(Player player)
+        {
+            if (Main.rand.NextBool(2))
+            {
+                Dust d = Dust.NewDustDirect(player.position, player.width, player.height, DustID.Electric, 0, 0, 100, Color.Green, 0.5f);
+                d.noGravity = true;
+                d.velocity *= 0.5f;   
+                Dust d2 = Dust.NewDustDirect(player.position, player.width, player.height, DustID.RedTorch, 0, 0, 100, Color.Red, 0.5f);
+                d2.noGravity = true;
+                d2.velocity *= 1.5f;
+            }
+        }
+
+        public override void OnChargeComplete(Player player)
+        {
+            int maxDamage = 2500;
+             Vector2 Direction = Main.MouseWorld - player.Center;
+                Direction.Normalize();
+                Vector2 Velocity = Direction * 15f;
+                Vector2 BaseSpawnLocation = player.Center + (Direction * 90f);
+
+             Projectile.NewProjectile(player.GetSource_FromThis(), BaseSpawnLocation, Velocity, ModContent.ProjectileType<DetroitSmashProj>(), maxDamage, 2f, player.whoAmI);
+                    Projectile.NewProjectile(player.GetSource_FromThis(), BaseSpawnLocation, Velocity, ModContent.ProjectileType<PunchAttackProj>(), 0, 0f, player.whoAmI);
+                    SoundEngine.PlaySound(new SoundStyle("MyHeroMod/Assets/Sounds/smash2") with { Volume = 0.5f }, player.position);
+
+            player.statLife -= (int)(0.25f * player.statLifeMax2);
+                    if (player.statLife <= 0)
+                    {
+                        var reason = PlayerDeathReason.ByCustomReason(Terraria.Localization.NetworkText.FromKey("Mods.MyHeroMod.DeathMessage", player.name));
+                        player.KillMe(reason,  maxDamage, 0);
+                    }
+                
+
+            PunchCameraModifier shake = new PunchCameraModifier(player.Center, Main.rand.NextVector2CircularEdge(1f, 1f), 10f, 15f, 20, 1000f, "FullCowlingShake");
+            Main.instance.CameraModifiers.Add(shake);
+            
+        }
+    }
+}
