19eac1afbec3b8da9148fb8f7a69a4a9c8015982
Fernando <fernandogomest94@gmail.com>
2026-09-02 21:00:05 -0400

    updating the state of mod tiles

--- content/Items/Placeable/NomuVatItem.cs
+++ content/Items/Placeable/NomuVatItem.cs
@@ -1,38 +1,38 @@
-// using Terraria;
-// using Terraria.ID;
-// using Terraria.ModLoader;
-// using MyHeroMod.content.Tiles.CraftingStations;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using MyHeroMod.content.Tiles.CraftingStations;
 
-// namespace MyHeroMod.content.Items.Placeable
-// {
-//     public class NomuVatItem : ModItem
-//     {
-//         public override void SetDefaults()
-//         {
-//             Item.width = 28;
-//             Item.height = 14;
-//             Item.maxStack = 99; 
+namespace MyHeroMod.content.Items.Placeable
+{
+    public class NomuVatItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 14;
+            Item.maxStack = 99; 
             
             
-//             Item.useTurn = true;
-//             Item.autoReuse = true;
-//             Item.useAnimation = 15;
-//             Item.useTime = 10;
-//             Item.useStyle = ItemUseStyleID.Swing;
-//             Item.consumable = true;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
             
             
-//             Item.createTile = ModContent.TileType<NomuVat>();
-//         }
+            Item.createTile = ModContent.TileType<NomuVat>();
+        }
 
       
-//         public override void AddRecipes()
-//         {
-//             CreateRecipe()
-//                 .AddIngredient(ItemID.IronBar, 10)
-//                 .AddIngredient(ItemID.Wood, 10)
-//                 .AddTile(TileID.WorkBenches)
-//                 .Register();
-//         }
-//     }
-// }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.IronBar, 10)
+                .AddIngredient(ItemID.Wood, 10)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+    }
+}
