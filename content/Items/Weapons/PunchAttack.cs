47929594d57bca549315a80ad6f43c2b4a41d46c
Fernando <fernandogomest94@gmail.com>
2026-09-04 12:35:41 -0400

    updating the punch attack

--- content/Items/Weapons/PunchAttack.cs
+++ content/Items/Weapons/PunchAttack.cs
@@ -1,9 +1,9 @@
-using MyHeroMod.content.System; // Para acessar SkillData/QuirkType
+using MyHeroMod.content.System; 
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using MyHeroMod.content;
-using MyHeroMod.content.Quirks.OFA9th.Projectiles; // Para TransformationPlayer
+using MyHeroMod.content.Quirks.OFA9th.Projectiles; 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria.GameContent;
@@ -16,126 +16,142 @@ namespace MyHeroMod.content.Items.Weapons
     {
         public override void SetDefaults()
         {
-            Item.damage = 14; 
+            Item.damage = 15; 
             Item.DamageType = DamageClass.Melee;
             Item.width = 30;
             Item.height = 30;
-            
-            
             
             Item.useTime = 10;      
             Item.useAnimation = 10; 
             Item.useStyle = ItemUseStyleID.Swing; 
             
-            Item.noMelee = true;   
+            Item.noMelee = false;   
             Item.knockBack = 4f;    
             Item.value = 0;
             Item.rare = ItemRarityID.White;
             Item.autoReuse = true;  
             
-             
             Item.shoot = ModContent.ProjectileType<PunchAttackProj>(); 
-                
-            
-            Item.shootSpeed = 25f;
+            Item.shootSpeed = 20f;
 
             Item.useTurn = true;    
             Item.noUseGraphic = true; 
         }
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-        {
-            
-                
-                Vector2 perturbedSpeed = velocity.RotatedByRandom(MathHelper.ToRadians(20)); 
-                float scale = 1f - (Main.rand.NextFloat() * 0.1f);
-                perturbedSpeed = perturbedSpeed * scale; 
 
-                
-                Vector2 offset = velocity.RotatedBy(MathHelper.PiOver2);
-                offset.Normalize();
-                
-                offset *= Main.rand.NextFloat(-16f, 16f);
-                
-                
-                Vector2 perturbedPosition = position + offset;
-
-                
-                int textureStyle = Main.rand.Next(1); 
-
-               
-
-                Projectile.NewProjectile(
-                    source, 
-                    perturbedPosition, 
-                    perturbedSpeed, 
-                    type, 
-                    damage, 
-                    knockback, 
-                    player.whoAmI, 
-                    textureStyle 
-                );
-
-                if (player.ownedProjectileCounts[ModContent.ProjectileType<PunchAnimProj>()] < 1)
-                
-    {
-        Projectile.NewProjectile(
-            source,
-            player.Center, 
-            velocity,      
-            ModContent.ProjectileType<PunchAnimProj>(),
-            0,             
-            0,             
-            player.whoAmI
-
-            
-        );
-
-                SoundEngine.PlaySound(new SoundStyle("MyHeroMod/Assets/Sounds/punchbarrage") 
-        { 
-            Volume = 0.50f,
-            MaxInstances = 1, 
-            SoundLimitBehavior = SoundLimitBehavior.IgnoreNew 
-        }, player.position);
-    }
-            
-
-            return false;
-
-            
-        }
-
-        
-        public override bool CanUseItem(Player player)
+        public override void HoldItem(Player player)
         {
             var modPlayer = player.GetModPlayer<TransformationPlayer>();
 
             
-            bool isPunchUser = modPlayer.HasActiveQuirk(QuirkType.OneForAll9th) || 
-                               modPlayer.HasActiveQuirk(QuirkType.OneForAll8th) || 
-                               modPlayer.HasActiveQuirk(QuirkType.Gearshift) || 
-                               modPlayer.HasActiveQuirk(QuirkType.Overclock);
-
-            return isPunchUser; 
+            switch (modPlayer.CurrentStage)
+            {
+                case QuirkStage.Initial:
+                    Item.scale = 1.0f;
+                    Item.shootSpeed = 20f;
+                    break;
+                case QuirkStage.Adequation:
+                    Item.scale = 1.2f;
+                    Item.shootSpeed = 23f;
+                    break;
+                case QuirkStage.Intermediate:
+                    Item.scale = 1.4f;
+                    Item.shootSpeed = 26f;
+                    break;
+                case QuirkStage.Advanced:
+                    Item.scale = 1.6f;
+                    Item.shootSpeed = 30f;
+                    break;
+                case QuirkStage.Final:
+                    Item.scale = 2.0f;
+                    Item.shootSpeed = 35f;
+                    break;
+            }
         }
-        
 
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            var modPlayer = player.GetModPlayer<TransformationPlayer>();
+
+           
+            float rangeMultiplier = modPlayer.CurrentStage switch
+            {
+                QuirkStage.Initial => 0.4f,       
+                QuirkStage.Adequation => 0.6f,
+                QuirkStage.Intermediate => 0.8f,
+                QuirkStage.Advanced => 1.0f,
+                QuirkStage.Final => 1.0f,         
+                _ => 0.6f
+            };
+
+            
+            Vector2 adjustedVelocity = velocity * rangeMultiplier;
+
+            Vector2 perturbedSpeed = adjustedVelocity.RotatedByRandom(MathHelper.ToRadians(20)); 
+            float scale = 1f - (Main.rand.NextFloat() * 0.1f);
+            perturbedSpeed = perturbedSpeed * scale; 
+
+            Vector2 offset = adjustedVelocity.RotatedBy(MathHelper.PiOver2);
+            offset.Normalize();
+            offset *= Main.rand.NextFloat(-16f, 16f);
+            
+            Vector2 perturbedPosition = position + offset;
+            int textureStyle = Main.rand.Next(1); 
+
+            Projectile.NewProjectile(
+                source, 
+                perturbedPosition, 
+                perturbedSpeed, 
+                type, 
+                damage, 
+                knockback, 
+                player.whoAmI, 
+                textureStyle 
+            );
+
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<PunchAnimProj>()] < 1)
+            {
+                Projectile.NewProjectile(
+                    source,
+                    player.Center, 
+                    velocity,       
+                    ModContent.ProjectileType<PunchAnimProj>(),
+                    0,             
+                    0,             
+                    player.whoAmI
+                );
+
+                SoundEngine.PlaySound(new SoundStyle("MyHeroMod/Assets/Sounds/punchbarrage") 
+                { 
+                    Volume = 0.50f,
+                    MaxInstances = 1, 
+                    SoundLimitBehavior = SoundLimitBehavior.IgnoreNew 
+                }, player.position);
+            }
+
+            return false;
+        }
         
         public override void ModifyWeaponDamage(Player player, ref StatModifier damage)
         {
             var modPlayer = player.GetModPlayer<TransformationPlayer>();
-            
-        
-            if (modPlayer.CurrentStage >= QuirkStage.Intermediate)
+       
+            float stageDamage = modPlayer.CurrentStage switch
             {
-                damage += 0.5f; 
-            
-            if (modPlayer.CurrentStage >= QuirkStage.Final)
-            {
-                damage += 1.0f; 
-            }
+                QuirkStage.Initial => 14f,      
+                QuirkStage.Adequation => 28f,    
+                QuirkStage.Intermediate => 45f, 
+                QuirkStage.Advanced => 75f,     
+                QuirkStage.Final => 130f,        
+                _ => 10f, 
+            };
+
+            damage.Flat += stageDamage;
         }
-        
+
+        public override void AddRecipes()
+        { 
+            CreateRecipe()
+                .Register();
+        }
     }
 }
-}
-
