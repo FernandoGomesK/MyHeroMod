f2e960d42837bcc27395f6a1965c2f0ade7289ff
Fernando <fernandogomest94@gmail.com>
2026-09-04 10:00:05 -0400

    added sound effects fot the nomu enemy, fixed some of the sprites of mei hatsume

--- content/Npcs/Enemies/Nomu/NomuNPC.cs
+++ content/Npcs/Enemies/Nomu/NomuNPC.cs
@@ -2,15 +2,25 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using System;
+using Terraria.Audio;
 
 namespace MyHeroMod.content.Npcs.Enemies.Nomu
 {
     public class NomuNPC : ModNPC
     {
+        public bool isBarraging = false;
+        
+        public int barrageCooldown = 180; 
+        
+        public int attackPhase = 0; 
+        public int phaseTimer = 0; 
+
+       
+        public int randomSfxTimer = 0;
+
         public override void SetStaticDefaults()
         {
-            
-            Main.npcFrameCount[Type] = 12; 
+            Main.npcFrameCount[Type] = 23; 
         }
 
         public override void SetDefaults()
@@ -23,29 +33,93 @@ namespace MyHeroMod.content.Npcs.Enemies.Nomu
             NPC.value = Item.buyPrice(silver: 5);
             NPC.knockBackResist = 0.5f;
 
-            
             NPC.aiStyle = NPCAIStyleID.Fighter; 
             AIType = NPCID.FaceMonster;
 
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCDeath1;
+
+            barrageCooldown = Main.rand.Next(180, 360);
+            
+            
+            randomSfxTimer = Main.rand.Next(180, 480);
+        }
+
+        public override bool PreAI()
+        {
+            
+            randomSfxTimer--;
+            if (randomSfxTimer <= 0)
+            {
+               
+                SoundEngine.PlaySound(new SoundStyle("MyHeroMod/Assets/Sounds/NomuSFX") with { Volume = 0.3f}, NPC.Center);
+                
+                
+                randomSfxTimer = Main.rand.Next(720, 1000);
+            }
+
+         
+            if (attackPhase == 1) 
+            {
+                NPC.velocity.X *= 0.60f; 
+                
+                phaseTimer--;
+                if (phaseTimer <= 0)
+                {
+                    NPC.velocity.X = NPC.direction * 15f; 
+                    
+                    SoundEngine.PlaySound(new SoundStyle("MyHeroMod/Assets/Sounds/whoosh"), NPC.Center);
+                    SoundEngine.PlaySound(new SoundStyle("MyHeroMod/Assets/Sounds/WooshSound"), NPC.Center);
+                    
+                    attackPhase = 2;
+                    phaseTimer = 360; 
+                    isBarraging = true; 
+                    
+                    SoundEngine.PlaySound(new SoundStyle("MyHeroMod/Assets/Sounds/PunchBarrageSFX") with { Volume = 0.6f}, NPC.Center);
+                }
+                
+                return false; 
+            }
+            else if (attackPhase == 2)
+            {
+                phaseTimer--;
+                if (phaseTimer <= 0)
+                {
+                    attackPhase = 0;
+                    isBarraging = false;
+                    barrageCooldown = Main.rand.Next(180, 360); 
+                }
+                
+                return true; 
+            }
+            else 
+            {
+                barrageCooldown--;
+                
+                if (barrageCooldown <= 0 && NPC.velocity.Y == 0)
+                {
+                    attackPhase = 1;
+                    phaseTimer = 60;
+                    return false; 
+                }
+            }
+
+            return true; 
         }
 
         public override void PostAI()
         {
-            
+            if (attackPhase == 1) return; 
+
             float maxSpeed = 20f; 
             float acceleration = 0.8f; 
 
-            
             if (NPC.velocity.Y == 0)
             {
-                
                 if (NPC.direction == 1 && NPC.velocity.X < maxSpeed)
                 {
                     NPC.velocity.X += acceleration;
                 }
-               
                 else if (NPC.direction == -1 && NPC.velocity.X > -maxSpeed)
                 {
                     NPC.velocity.X -= acceleration;
@@ -55,40 +129,54 @@ namespace MyHeroMod.content.Npcs.Enemies.Nomu
 
         public override void FindFrame(int frameHeight)
         {
-           
             NPC.spriteDirection = NPC.direction;
 
-            int frameStanding = 0;
-            int frameJumping = 3;
+            int frameOffset = isBarraging ? 11 : 0; 
+            
+            int frameStanding = 0 + frameOffset;
+            int frameJumping = 3; 
+            
+            int walkStart = 1 + frameOffset;
+            int walkEnd = 11 + frameOffset; 
 
-          
             if (NPC.velocity.Y != 0f)
             {
                 NPC.frame.Y = frameJumping * frameHeight;
                 NPC.frameCounter = 0; 
             }
-            
             else if (Math.Abs(NPC.velocity.X) < 0.1f)
             {
                 NPC.frame.Y = frameStanding * frameHeight;
                 NPC.frameCounter = 0;
             }
-            
             else
             {
-            
+                int currentFrameIndex = NPC.frame.Y / frameHeight;
+                if (currentFrameIndex < walkStart || currentFrameIndex > walkEnd)
+                {
+                    NPC.frame.Y = walkStart * frameHeight;
+                }
+
                 NPC.frameCounter += Math.Abs(NPC.velocity.X); 
 
                 if (NPC.frameCounter >= 6.0) 
                 {
                     NPC.frame.Y += frameHeight;
                     NPC.frameCounter = 0;
-                    
                 }
 
-                if (NPC.frame.Y > 10 * frameHeight || NPC.frame.Y < 1 * frameHeight)
+                
+                int currentFrame = NPC.frame.Y / frameHeight;
+                if (currentFrame == 4 + frameOffset || currentFrame == 9 + frameOffset)
                 {
-                    NPC.frame.Y = 1 * frameHeight; 
+                    
+                    SoundEngine.PlaySound(new SoundStyle("MyHeroMod/Assets/Sounds/NomuFootstepSFX") with { Volume = 0.2f}, NPC.Center);
+                }
+
+                currentFrameIndex = NPC.frame.Y / frameHeight;
+                if (currentFrameIndex > walkEnd || currentFrameIndex < walkStart)
+                {
+                    NPC.frame.Y = walkStart * frameHeight; 
                 }
             }
         }
