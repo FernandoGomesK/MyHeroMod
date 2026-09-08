58458f5a0fde93ae51d7a925411a94149b5ed157
FernandoGomesK <fernandogomest94@gmail.com>
2026-09-07 22:59:57 -0400

    adding some custom armor testing

---
+++ content/Items/Armor/Spider/BrandNewDay/BNDBreastplate.cs
@@ -0,0 +1,62 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+using MyHeroMod.content.System;
+
+namespace MyHeroMod.content.Items.Armor.Spider.BrandNewDay{
+    [AutoloadEquip(EquipType.Body)]
+    public class BNDBreastplate : ModItem
+    {
+        public static int FemaleBodySlot;
+
+        public static int CapeSlotID { get; private set; }
+
+        public override void Load()
+        {
+            if (Main.netMode == NetmodeID.Server) return;
+
+            
+            
+        }
+
+                
+
+
+    
+    public override void SetStaticDefaults()
+        {
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            
+        }
+    public override void SetMatch(bool male, ref int equipSlot, ref bool robes)
+        {
+            if (!male && FemaleBodySlot != -1)
+            {
+                equipSlot = FemaleBodySlot;
+            }
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 18; 
+            Item.height = 18;
+            Item.value = Item.sellPrice(gold: 1);
+            Item.rare = ItemRarityID.Yellow;
+            Item.defense = 20; 
+        }
+        public override void UpdateEquip(Player player)
+        {
+            
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddRecipeGroup(RecipeSystem.IronAndLeadGroup, 20)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+}
+}
