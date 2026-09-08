92e228ae2721b7d195c1fe7ca1699fe61022f6f5
Fernando <fernandogomest94@gmail.com>
2026-09-08 08:19:12 -0400

    making so that you can only obtain the one for all 9th vestiges if you only have one for all 9th

--- content/Quirks/OFA9th/OneForAll9th.Helper.cs
+++ content/Quirks/OFA9th/OneForAll9th.Helper.cs
@@ -7,44 +7,46 @@ namespace MyHeroMod.content.Quirks.OFA9th
     {
         public void UnlockQuirks()
         {
+            var transPlayer = Player.GetModPlayer<TransformationPlayer>();
 
-        var transPlayer = Player.GetModPlayer<TransformationPlayer>();
-    
+            InternalQuirks.Clear(); 
 
-        InternalQuirks.Clear(); 
-        
+            if (transPlayer.HasActiveQuirk(QuirkType.OneForAll9th))
+            {
+                if (transPlayer.ActiveQuirks.Count > 1)
+                {
+                    if (transPlayer.CurrentStage >= QuirkStage.Initial)
+                        InternalQuirks.Add(QuirkType.OneForAll9th); 
+                }
+                else 
+                {
+                    if (transPlayer.CurrentStage >= QuirkStage.Initial)
+                    {
+                        InternalQuirks.Add(QuirkType.OneForAll9th); 
+                    }
 
+                 
 
-        if (transPlayer.HasActiveQuirk(QuirkType.OneForAll9th))
-    {
+                    if (transPlayer.CurrentStage >= QuirkStage.Intermediate)
+                    {
+                        
+                        InternalQuirks.Add(QuirkType.DangerSense);
+                        InternalQuirks.Add(QuirkType.BlackWhip);
+                    }
 
-        // if (transPlayer.ActiveQuirks >= 1);
-        
-        if (transPlayer.CurrentStage >= QuirkStage.Initial)
-            InternalQuirks.Add(QuirkType.OneForAll9th); 
+                    if (transPlayer.CurrentStage >= QuirkStage.Advanced)
+                    {
+                        InternalQuirks.Add(QuirkType.Float);
+                        InternalQuirks.Add(QuirkType.SmokeScreen);
+                        InternalQuirks.Add(QuirkType.FaJin);
+                    }
 
-        if (transPlayer.CurrentStage >= QuirkStage.Adequation)
-            
-
-        if (transPlayer.CurrentStage >= QuirkStage.Intermediate)
-            InternalQuirks.Add(QuirkType.DangerSense);
-            InternalQuirks.Add(QuirkType.BlackWhip);
-
-        if (transPlayer.CurrentStage >= QuirkStage.Advanced)
-        {
-            InternalQuirks.Add(QuirkType.Float);
-            InternalQuirks.Add(QuirkType.SmokeScreen);
-            InternalQuirks.Add(QuirkType.FaJin);
-        }
-
-        if (transPlayer.CurrentStage >= QuirkStage.Final)
-        {
-            
-            InternalQuirks.Add(QuirkType.Gearshift);
+                    if (transPlayer.CurrentStage >= QuirkStage.Final)
+                    {
+                        InternalQuirks.Add(QuirkType.Gearshift);
+                    }
+                }
+            }
         }
     }
-
-    
-        }
-
-}}
+}
